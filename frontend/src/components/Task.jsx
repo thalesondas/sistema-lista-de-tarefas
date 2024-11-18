df167e5e74a4a670aa3efb87d3849a7cb3c6f8ca
Thales <thalesondas0@gmail.com>
2024-11-18 07:35:42 -0300

    Integrando frontend com o backend

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -2,9 +2,23 @@ import { Container } from 'react-bootstrap';
 import '../assets/Task.css'
 
 const Task = (props) => {
+
+    const deadline = new Date(props.deadline).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+
+    const cost = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(props.cost);
+
     return(
-        <Container className='container-task my-3 py-3 w-75 fs-4 d-flex justify-content-center'>
+        <Container className='container-task my-3 py-3 w-75 fs-4 d-flex justify-content-evenly'>
             <span>{props.name}</span>
+            <span>{cost}</span>
+            <span>{deadline}</span>
         </Container>
     )
 }
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,17 +1,33 @@
-import { Container } from 'react-bootstrap'
+import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import Task from '../components/Task';
-import '../assets/MainPage.css'
+import axios from 'axios';
+import '../assets/MainPage.css';
 
 const MainPage = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchTasks = async() => {
+            try{
+                const response = await axios.get('http://localhost:5000/');
+                setTasks(response.data);
+            } catch(err){
+                console.log(err);
+            }
+        }
+
+        fetchTasks();
+    }, []);
+
     return(
         <Container className='container-base d-flex flex-column justify-content-start align-items-center'>
             <h1 className='py-4'>Desafio FATTO</h1>
             <h2 className='pb-2'>Lista de Tarefas</h2>
-            <Task name="Task1"/>
-            <Task name="Task2"/>
-            <Task name="Task3"/>
-            <Task name="Task4"/>
-            <Task name="Task5"/>
+            {tasks.map((task) => (
+                <Task name={task.name} cost={task.cost} deadline={task.deadline}/>
+            ))}
         </Container>
     )
 }
