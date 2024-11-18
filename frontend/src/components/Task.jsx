6e08135a9a5e5682d8b6fb42bbead2c9216f8d55
Thales <thalesondas0@gmail.com>
2024-11-18 08:12:40 -0300

    Lógica para deixar o fundo amarelado caso o custo for maior do que 1000 adicionado

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -14,15 +14,19 @@ const Task = (props) => {
         currency: 'BRL'
     }).format(props.cost);
 
+    const moreThan1000 = () => {
+        return props.cost >= 1000;
+    };
+
     return(
-        <Container xs={10} className='container-task my-3 py-3 w-75 fs-4 d-flex justify-content-evenly align-items-center'>
+        <Container xs={10} className={`container-task my-3 py-3 w-75 fs-4 d-flex align-items-center ${moreThan1000() ? "bg-warning" : ""}`}>
             <Col className='d-flex justify-content-evenly'>
                 <span>{props.name}</span>
                 <span>{cost}</span>
                 <span>{deadline}</span>
             </Col>
             <Col xs={2}>
-                <Button className='me-3 btn-warning'><i class="bi bi-pencil-square fs-5"></i></Button>
+                <Button className='me-3 btn-primary'><i class="bi bi-pencil-square fs-5"></i></Button>
                 <Button className='btn-danger'><i class="bi bi-x-square fs-5"></i></Button>
             </Col>
         </Container>
