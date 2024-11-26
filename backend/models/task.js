883a9ab83678fbf39ebd4f0dfc3609eac4298ebb
Thales <thalesondas0@gmail.com>
2024-11-26 01:23:48 -0300

    Não deixando as tarefas terem 2 nomes iguais

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   cost: { type: Number, required: true }, 
   deadline: { type: Date, required: true },
   order: { type: Number, required: true }
